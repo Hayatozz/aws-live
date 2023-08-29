122a057cc101151b199c7e3ba7f2fdf99b6d9eba
Hayatozz <99627850+Hayatozz@users.noreply.github.com>
2023-08-29 11:41:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.csewlekqj9mb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yapzhaoteng-employee"
+customregion = "us-east-1"
+
